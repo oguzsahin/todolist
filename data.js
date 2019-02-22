daa5437e1530e8272e25edd4b5200d66185bd016
Oguz Sahin <ozinet@hotmail.com>
2019-02-22 17:46:53 +0300

    Initial commit

---
+++ data.js
@@ -0,0 +1,31 @@
+
+//jshint esversion:6
+// jshint expr: true
+
+module.exports.getDate = function() {
+
+let today = new Date();
+
+
+let options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long"
+};
+
+return today.toLocaleDateString("en-US", options);
+
+};
+
+module.exports.getDay = function() {
+
+let today = new Date();
+
+
+let options = {
+  weekday: "long",
+};
+
+return today.toLocaleDateString("en-US", options);
+
+};
